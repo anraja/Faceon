57addee2399cbfcb6dec585e0190f54a2fe1bb98
Susapp <antonsch@kth.se>
2015-02-17 12:51:05 +0100

    Done
    Added back functionallity

--- app/src/main/java/com/example/anitha/faceon/loose.java
+++ app/src/main/java/com/example/anitha/faceon/loose.java
@@ -1,17 +1,32 @@
 package com.example.anitha.faceon;
 
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class loose extends ActionBarActivity {
+public class Loose extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loose);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        Button back_button = (Button) findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("Loose", "Clicked to go back to group");
+                finish();
+            }
+        });
     }
 
 
