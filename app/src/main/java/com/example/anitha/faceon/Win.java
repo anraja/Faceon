bc175aa0364555281eb0e90b69031b2a94314b94
Susapp <antonsch@kth.se>
2015-02-17 10:23:21 +0100

    Partly finished
    Looks good but no functionallity

--- app/src/main/java/com/example/anitha/faceon/Win.java
+++ app/src/main/java/com/example/anitha/faceon/Win.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.util.Log;
 
